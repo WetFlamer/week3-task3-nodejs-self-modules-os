24ef9dd76107564e28b1e51074e13fd91f05198a
WetFlamer <xamidchabaev04@gmail.com>
2022-11-07 13:10:43 +0300

    Task Completed

---
+++ index.js
@@ -0,0 +1,4 @@
+import result from 'os'
+import {r} from './welcome.js'
+console.log(result.homedir(), result.hostname(), result.version(), result.type())
+console.log(r())
---
+++ welcome.js
@@ -0,0 +1,5 @@
+import rs from 'os'
+function r () {
+    return `Привет, юзер. Вижу ты зашел с ${rs.type}`
+}
+export{r}
